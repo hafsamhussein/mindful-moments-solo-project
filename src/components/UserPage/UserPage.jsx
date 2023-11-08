fdb0e5ebd01af90f0fb238203cb632a1a47a7ea0
Hafsa Hussein <hafsahussein2014@gmail.com>
2023-11-08 16:16:41 -0600

    embedded a youtube video on login page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import YouTube from 'react-youtube'; 
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const opts = {
+    height: '380', 
+    width: '660', 
+    playerVars: {
+      
+      autoplay: 0, // Auto-play disabled (0) or enabled (1)
+    },
+  };
+  const videoId = 'SYMsBN_DkfY'; 
+
   return (
     <div className="container">
       <h2>Welcome to Mindful Moments, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+      <p>{user.id}</p>
+      <YouTube videoId={videoId} opts={opts} />
+
       <LogOutButton className="btn" />
     </div>
   );
@@ -16,4 +29,6 @@ function UserPage() {
 
 // this allows us to use <App /> in index.js
 export default UserPage;
+
+
 
