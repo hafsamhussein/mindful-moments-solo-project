cb92669079d79c84f113944e33643c8ef38de012
Hafsa Hussein <hafsahussein2014@gmail.com>
2023-11-08 19:02:38 -0600

    final changes

--- src/components/MomentsPage/MomentsPage.jsx
+++ src/components/MomentsPage/MomentsPage.jsx
@@ -1,90 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { useDispatch } from "react-redux";
-// import { useHistory } from "react-router-dom";
-// import './MomentsPage.css'; 
-
-// function MomentsPage() {
-//   const dispatch = useDispatch();
-//   const history = useHistory();
-//   const [momentsList, setMomentsList] = useState([]);
-  
-
-//   useEffect(() => {
-//     fetchMoments();
-//   }, []);
-
-//   const fetchMoments = async () => {
-//     try {
-//       const response = await fetch("/api/moments");
-//       if (!response.ok) {
-//         throw new Error("Network response was not OK");
-//       }
-//       const results = await response.json();
-//       setMomentsList(results);
-//     } catch (error) {
-//       console.error(error);
-//       alert("Something went wrong fetching moments.");
-//     }
-//   };
-
-//   const deleteMoment = async (id) => {
-//     try {
-//       const response = await fetch(`/api/moments/${id}`, { method: "DELETE" });
-//       if (!response.ok) {
-//         throw new Error("Network response was not OK");
-//       }
-//       fetchMoments();
-//     } catch (error) {
-//       console.error(error);
-//       alert("Something went wrong deleting the moment.");
-//     }
-//   };
-
-//   const editMoment = (id) => {
-//     const momentToEdit = momentsList.find((moment) => moment.id === id);
-//     if (!momentToEdit) {
-//       console.error(`No moment found with id: ${id}`);
-//       return;
-//     }
-
-//     dispatch({
-//       type: "SET_MOMENT",
-//       payload: momentToEdit,
-//     });
-
-//     history.push(`/EditMoment/${id}`);
-//   };
-
-//   const addNewMoment = () => {
-//     history.push("/AddNewMoment");
-//   };
-
-//   const formatDate = (isoString) => {
-//     return isoString.split('T')[0];
-//   };
-
-//   return (
-//     <div className="container">
-      
-//       <h2>Your Moments</h2>
-//       {momentsList.map((moment) => (
-//         <div key={moment.id} className="moment-card">
-          
-//           <h3>{moment.name}</h3>
-//           <p>{moment.notes}</p>
-//           <p>Date: {formatDate(moment.date)}</p>
-//           <img src={moment.photo_url} alt={moment.name} />
-//           <button onClick={() => editMoment(moment.id)}>Edit</button>
-//           <button onClick={() => deleteMoment(moment.id)}>Delete</button>
-//         </div>
-//       ))}
-//       <button onClick={addNewMoment}>Add New Moment</button>
-//     </div>
-//   );
-// }
-
-// export default MomentsPage;
-
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import './MomentsPage.css';
@@ -167,43 +80,58 @@ function MomentsPage() {
   const formatDate = (isoString) => {
         return isoString.split('T')[0];
       };
+      const [showFavorites, setShowFavorites] = useState(false);
 
-  const renderMoment = (moment, isFavoriteList = false) => (
-    <div key={moment.id} className="moment-card">
-      <h3>{moment.name}</h3>
-      <p>{moment.notes}</p>
-      <p>Date: {formatDate(moment.date)}</p>
-      <img src={moment.photo_url} alt={moment.name} />
-      {!isFavoriteList && (
-        <>
-          <button onClick={() => editMoment(moment.id)}>Edit</button>
-          <button onClick={() => deleteMoment(moment.id)}>Delete</button>
-        </>
-      )}
-      <button onClick={() => toggleFavorite(moment)}>
-        {favoriteMoments.includes(moment.id) ? 'Unfavorite' : 'Favorite'}
-      </button>
-    </div>
-  );
+      const toggleFavoritesVisibility = () => {
+        setShowFavorites(!showFavorites);
+      };
+      const renderMoment = (moment, isFavoriteList = false) => (
+        <div key={moment.id} className="moment-card">
+          <img src={moment.photo_url} alt={moment.name} />
+          <div className="moment-card-content">
+            <h3>{moment.name}</h3>
+            <p>{moment.notes}</p>
+            <p>Date: {formatDate(moment.date)}</p>
+          </div>
+          <div className="moment-card-actions">
+            {!isFavoriteList && (
+              <>
+                <button onClick={() => editMoment(moment.id)}>Edit</button>
+                <button onClick={() => deleteMoment(moment.id)}>Delete</button>
+              </>
+            )}
+            <button onClick={() => toggleFavorite(moment)}>
+              {favoriteMoments.includes(moment.id) ? 'Unfavorite' : 'Favorite'}
+            </button>
+          </div>
+        </div>
+      );
 
   return (
-    <div className="container">
+    <div className={`container ${showFavorites ? 'show-favorites' : ''}`}>
       <h2>Your Moments</h2>
       <div className="moments-list">
         {momentsList.map((moment) => renderMoment(moment))}
       </div>
       <button onClick={addNewMoment}>Add New Moment</button>
+      <button onClick={toggleFavoritesVisibility}>
+        {showFavorites ? 'Hide Favorites' : 'View Favorites'}
+      </button>
       
-      <hr />
-      
-      <h2>Your Favorite Moments</h2>
-      <div className="favorites-list">
-        {momentsList
-          .filter((moment) => favoriteMoments.includes(moment.id))
-          .map((moment) => renderMoment(moment, true))}
-      </div>
+      {showFavorites && (
+        <>
+          <hr />
+          <h2>Your Favorite Moments</h2>
+          <div className="favorites-list">
+            {momentsList
+              .filter((moment) => favoriteMoments.includes(moment.id))
+              .map((moment) => renderMoment(moment, true))}
+          </div>
+        </>
+      )}
     </div>
   );
-}
+  
+            }
 
 export default MomentsPage;
